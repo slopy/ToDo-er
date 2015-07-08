175c893cbec0442a954d8fa4346459613ce38512
Michał Marzec <=>
2015-07-09 00:03:50 +0200

    devise with angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 // angular.module('todoer', [])
 
-angular.module('todoer', ['ui.router', 'templates'])
+angular.module('todoer', ['ui.router', 'templates', 'Devise'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -20,7 +20,27 @@ function($stateProvider, $urlRouterProvider) {
           goalPromise: ['goals', function(posts){
             return goals.getAll(); }]
         }
-	});
+	   })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthController',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthController',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    });
 
   $urlRouterProvider.otherwise('home');
 }])
