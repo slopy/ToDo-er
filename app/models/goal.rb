815309ec3f7f69ea518f259c3c751d2b8aecec94
Michał Marzec <michal.marzec@polcode.net>
2015-07-10 12:33:14 +0200

    cleanup

--- app/models/goal.rb
+++
@@ -1,9 +0,0 @@
-class Goal < ActiveRecord::Base
-
-    belongs_to :user
-
-    # validations:
-    validates :user, presence: true
-    validates :title, presence: true, length: { in: 6..30 }
-    validates :description, presence: true, length: { in: 3..100 }
-end
--- spec/controllers/goals_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe GoalsController do
-
-end
